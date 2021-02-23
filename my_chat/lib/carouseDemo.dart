2007c592103b1a0c63f212d177c935730fe1e2db
VanDu97 <nguyenvandu199740@gmail.com>
2021-02-23 13:44:34 +0700

    update navigation and GestureDetector and Scaffold and Padding

--- my_chat/lib/carouseDemo.dart
+++ my_chat/lib/carouseDemo.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+
 import 'package:carousel_slider/carousel_controller.dart';
 import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/src/widgets/scroll_view.dart';
+import 'dart:math';
+import 'detailItem.dart';
 
 final List<String> imgList = [
   'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
@@ -21,42 +27,118 @@ class CarouselWithIndicatorDemo extends StatefulWidget {
 
 class CarouseDemoImage extends State<CarouselWithIndicatorDemo> {
   int _current = 0;
+  int _selectedIndex = 0;
   CarouselController buttonCarouselController = CarouselController();
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  final List<int> entries = <int>[
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20
+  ];
+
+  final List<Widget> _widgetOptions = <Widget>[
+    ListView.builder(
+        padding: EdgeInsets.all(10),
+        // itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DetailItem()));
+              },
+              child: Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    border: Border.all(width: 0.2, color: Colors.black87),
+                    borderRadius: BorderRadius.circular(6)),
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(6),
+                        child: Image.network(
+                            'https://source.unsplash.com/random',
+                            fit: BoxFit.fitWidth,
+                            height: MediaQuery.of(context).size.height * 0.2,
+                            width: MediaQuery.of(context).size.width)),
+                    Padding(
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        child: Row(
+                          children: [
+                            Image.asset('lib/assets/ic_calendar.png',
+                                color: Colors.red,
+                                fit: BoxFit.cover,
+                                height: 20),
+                            SizedBox(width: 5),
+                            Text('23/02/2021 HN 10:00 - 11:00',
+                                style: TextStyle(color: Colors.red)),
+                          ],
+                        )),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Text('Buy Nothing Day',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Text(
+                          'In many wealthy countries, people celebrate an unusual holiday: Buy Nothing Day. Why would people choose NOT to buy things? Robin Basselin and Adam Navis look at this holiday.',
+                          style: TextStyle(color: Colors.black)),
+                    ),
+                  ],
+                ),
+              ));
+        }),
+    Text('Index 1 : Business', style: optionStyle),
+    Text('Index 2 : School', style: optionStyle),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Home')),
-        body: Column(children: [
-          CarouselSlider(
-            carouselController: buttonCarouselController,
-            items: imageSliders,
-            options: CarouselOptions(
-                autoPlay: false,
-                enlargeCenterPage: true,
-                aspectRatio: 2.0,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _current = index;
-                  });
-                }),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: imgList.map((url) {
-              int index = imgList.indexOf(url);
-              return Container(
-                  width: 10,
-                  height: 10,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: _current == index
-                          ? Color.fromRGBO(255, 0, 0, 1)
-                          : Color.fromRGBO(0, 0, 0, 0.4)));
-            }).toList(),
-          ),
-        ]));
+      backgroundColor: Colors.white,
+      body: Container(
+        child: SafeArea(child: _widgetOptions.elementAt(_selectedIndex)),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.business), label: 'Business'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School')
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
 
---
+++ my_chat/lib/detailItem.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DetailItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Buy Nothing Day')));
+  }
+}
